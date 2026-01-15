f1a105eb14df864e8dee751786ec84dad0439c4a
jessica <jessica.isma01@gmail.com>
2026-01-15 13:51:31 -0300

    First Commit Android

--- app/src/main/java/com/example/evenoroddnumber/MainActivity.kt
+++ app/src/main/java/com/example/evenoroddnumber/MainActivity.kt
@@ -5,20 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,83 +17,67 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Home()
-
         }
     }
 }
 
 @Composable
-fun Home(){
+fun Home() {
 
     var number by remember { mutableStateOf("") }
     var result by remember { mutableStateOf("") }
 
-    fun verifyNumber(){
+    fun verifyNumber() {
         val value = number.toIntOrNull()
-        result = when{
-            value == null ->" Please enter a valid number!"
-            value % 2 == 0 ->"$ Even value"
-            else -> "$ Odd number"
-
+        result = when {
+            value == null -> "Please enter a valid number."
+            value % 2 == 0 -> "Even number"
+            else -> "Odd number"
         }
     }
 
     Column(
-        modifier = Modifier.fillMaxSize().background(color = Color.White),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         OutlinedTextField(
             value = number,
-            onValueChange = {
-                number = it
-            },
-            label = {
-                Text(text = "Enter a Number")
-            },
-            
+            onValueChange = { number = it },
+            label = { Text(text = "Enter a number") },
             modifier = Modifier.fillMaxWidth(0.5f),
             maxLines = 1,
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
 
         Button(
-            onClick = {
-                verifyNumber()
-
-            },
+            onClick = { verifyNumber() },
             modifier = Modifier.padding(vertical = 20.dp)
-
         ) {
-
-
             Text(
                 text = "Verify",
                 fontSize = 18.sp
             )
-
         }
+
         Text(
-            text ="Result",
+            text = result,
             fontSize = 25.sp
         )
     }
-
 }
 
-
-
-@Preview
+@Preview(showBackground = true)
 @Composable
-private fun HomePreview(){
+fun HomePreview() {
     Home()
-
 }
